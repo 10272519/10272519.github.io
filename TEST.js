3455d9db8d514f705801c6bb68be8f04ca6dd9b0
10272519 <84452311+10272519@users.noreply.github.com>
2021-05-19 13:11:49 +0100

    Add files via upload

---
+++ TEST.js
@@ -0,0 +1,256 @@
+//Form button function to show popup 
+function msg(){	
+	var x=document.forms["message"]["comments"].value;
+	var y=document.forms["message"]["name"].value;
+	var z=document.forms["message"]["email"].value;
+		if (x=="")
+		{alert("Please enter your message");}
+	
+		else if (y=="")
+		{alert("Please enter your name");}
+	
+		else if (z=="")
+		{alert("Please enter a valid email address");}
+	
+		else
+		{alert("Your message has been sent");}
+}
+	
+//Spyware page functions
+function spy1(){
+	var x = document.getElementById("whatis");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function spy2(){
+	var x = document.getElementById("how");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function spy3(){
+	var x = document.getElementById("howrecog");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}	
+function spy4(){
+	var x = document.getElementById("prevent");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}	
+function spy5(){
+	var x = document.getElementById("example");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}	
+function spy6(){
+	var x = document.getElementById("reference");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+
+//Viruses page functions
+function virus1(){
+	var x = document.getElementById("whatis");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function virus2(){
+	var x = document.getElementById("howdoes");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function virus3(){
+	var x = document.getElementById("howdo");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function virus4(){
+	var x = document.getElementById("howcan");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function virus5(){
+	var x = document.getElementById("reference");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+
+//Ransomware page functions
+function ransom1(){
+	var x = document.getElementById("whatis");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function ransom2(){
+	var x = document.getElementById("howdoes");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function ransom3(){
+	var x = document.getElementById("howto");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function ransom4(){
+	var x = document.getElementById("example");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+function ransom5(){
+	var x = document.getElementById("reference");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+}
+
+//malware page functions
+function mal1(){
+		var x = document.getElementById("whatis");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+	}
+function mal2(){
+		var x = document.getElementById("type");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+	}			
+function mal3(){
+		var x = document.getElementById("prevent");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+	}
+function mal4(){
+		var x = document.getElementById("example");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+	}			
+function mal5(){
+		var x = document.getElementById("reference");
+		if (x.style.display === "none")
+		{
+			x.style.display = "block";
+		}
+		else
+		{
+			x.style.display = "none";
+		}
+	}
